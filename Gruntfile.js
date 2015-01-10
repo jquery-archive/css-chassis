2247d1197ac161bc5ce17a03f256b9b8c03a50d9
MichaelArestad <marestad@gmail.com>
2015-01-10 12:03:21 -0700

    Build: Added SVG placeholder icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function( grunt ) {
 grunt.loadNpmTasks( "grunt-contrib-jshint" );
 grunt.loadNpmTasks( "grunt-html" );
 grunt.loadNpmTasks( "grunt-jscs" );
+grunt.loadNpmTasks( "grunt-svgmin" );
+grunt.loadNpmTasks( "grunt-svgstore" );
+grunt.loadNpmTasks( "grunt-contrib-watch" );
 
 grunt.initConfig({
 	jshint: {
@@ -13,6 +16,55 @@ grunt.initConfig({
 	},
 	jscs: {
 		all: [ "*.js" ]
+	},
+
+	// minifies SVGs
+	svgmin: {
+		options: {
+			plugins: [
+				{
+					removeViewBox: false
+				}, {
+					removeUselessStrokeAndFill: false
+				}
+			]
+		},
+		dist: {
+			files: [ {
+				expand: true,
+				cwd: "svg-source",
+				src: [ "*.svg" ],
+				dest: "svg-min/",
+				ext: ".svg"
+			} ]
+		}
+	},
+
+	// Combines SVGs into single file
+	svgstore: {
+		defaults: {
+			options: {
+				prefix: "icon-", // This will prefix each ID
+				svg: { // will be added as attributes to the resulting SVG
+					viewBox: "0 0 24 24",
+					xmlns: "http://www.w3.org/2000/svg"
+				},
+				cleanup: [ "style", "fill", "id" ]
+			},
+			files: {
+				"svg-set/icons.svg": [ "svg-min/*.svg" ]
+			}
+		}
+	},
+
+	watch: {
+		svg: {
+			files: [ "svg-source/**/*.svg" ],
+			tasks: [ "svgmin", "svgstore" ],
+			options: {
+				spawn: false
+			}
+		}
 	}
 });
 
@@ -33,6 +85,7 @@ grunt.registerTask( "update-authors", function() {
 	});
 });
 
-grunt.registerTask( "default", [ "jshint", "jscs" ] );
+grunt.registerTask( "default", [ "watch" ] );
+grunt.registerTask( "lint", [ "jshint", "jscs" ] );
 
 };
