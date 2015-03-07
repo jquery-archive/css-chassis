56b3c25831ec44aeb7285711c9671bd4539c6c04
Rohit Mulange <rohit@rohit.codes>
2015-03-07 23:11:33 +0530

    Performance: Style correction and Optimization for topcoat tests

--- performance/frameworks/topcoat.js
+++ performance/frameworks/topcoat.js
@@ -1,57 +1,56 @@
 module.exports = {
-    css: [
-    "//cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/css/topcoat-desktop-dark.css",
-    "//cdnjs.cloudflare.com/ajax/libs/topcoat-icons/0.2.0/font/icomatic.css"
-    ],
-    button: {
-        generator: function( options ) {
-            var button = "<button class='";
-            var icon = "";
-            var buttonText = " Button";
-            var buttonClass = 'topcoat' + options.type + '-button'+ options.size;
+	css: [
+		"//cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/css/topcoat-desktop-dark.css",
+		"//cdnjs.cloudflare.com/ajax/libs/topcoat-icons/0.2.0/font/icomatic.css"
+	],
+	button: {
+		generator: function( options ) {
+			var button = "<button class='",
+			buttonText = options.type ? "" : " Button",
+			buttonClass = "topcoat" + options.type + "-button" + options.size,
+			icon = "",
+			iconClass = "topcoat-icon";
 
-            //Icon Buttons donot support the cta button type
-            if( options.type !== '-icon' || options.state !== '--cta' ) {
-                buttonClass = buttonClass + options.state;
-            }
-            button = button + buttonClass + "'>";
+			// Add icon only if it is a icon-button or there is an icon set
+			if ( options.icon || options.type ) {
 
-            //Add icon only if it is a icon-button or there is an icon set
-            if( options.icon !== "" || options.type === "-icon" ) {
-                icon = "<span class='";
-                iconClass="topcoat-icon";
+				// Icon butons donot support cta type
+				if ( !options.type || options.state !== "--cta" ) {
+					buttonClass += options.state;
+				}
 
-                //If it is an icon button, add size to class and reset buttonText
-                if( options.type === "-icon" ) {
-                    buttonText = "";
-                    iconClass = iconClass+options.size;
-                }
-                icon = icon + iconClass +" icomatic'>" + options.icon + "</span>";
-            }
-            return button + icon + buttonText + "</button>";
-        },
-        variations: {
-            type: [
-                "",
-                "-icon",
-            ],
-            size: [
-                "",
-                "--large",
-            ],
-            state: [
-                "",
-                "--cta",
-                "--quiet",
+				// If it is an icon button, add size to class
+				if ( options.type ) {
+					iconClass += options.size;
+				}
+				icon = "<span class='" + iconClass + " icomatic'>" + options.icon +
+					"</span>";
+			}
 
-            ],
-            icon: [
-                "",
-                "location",
-                "home",
-                "alert"
-            ]
+			return button + buttonClass + "'>" + icon + buttonText + "</button>";
+		},
+		variations: {
+			type: [
+				"",
+				"-icon"
+			],
+			size: [
+				"",
+				"--large"
+			],
+			state: [
+				"",
+				"--cta",
+				"--quiet"
 
-        }
-    },
+			],
+			icon: [
+				"",
+				"location",
+				"home",
+				"alert"
+			]
+
+		}
+	}
 };
