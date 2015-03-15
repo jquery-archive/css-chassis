842bdb43e24c6cc09b548a2a7ad8dc9a6cf322e9
Prasun Anand <prasunanand.bitsp@gmail.com>
2015-03-16 03:19:37 +0530

    Performace:Kendo-ui buttons added

---
+++ performance/frameworks/kendo-ui.js
@@ -0,0 +1,33 @@
+module.exports = {
+	css: [ "//cdn.kendostatic.com/2014.1.318/styles/kendo.common.min.css",
+			"//cdn.kendostatic.com/2014.1.318/styles/kendo.default.min.css",
+			"//cdn.kendostatic.com/2014.1.318/styles/kendo.dataviz.min.css",
+			"//cdn.kendostatic.com/2014.1.318/styles/kendo.dataviz.default.min.css"
+	],
+	button: {
+		generator: function( options ) {
+			var button = "<button class='k-button " + options.state + " " +
+				options.disabled + "' >";
+			if ( options.icon ) {
+				button = button + "<span class='k-sprite k-icon " + options.icon + "'></span>";
+			}
+			return button + " Button </button>";
+		},
+		variations: {
+			state: [
+				"",
+				"k-primary"
+			],
+			disabled: [
+				"",
+				"k-state-disabled"
+			],
+			icon: [
+				false,
+				"k-i-funnel",
+				"k-i-funnel-clear",
+				"k-i-refresh"
+			]
+		}
+	}
+};
--- tasks/options/perfjankie.js
+++ tasks/options/perfjankie.js
@@ -36,7 +36,9 @@ module.exports = {
 				"http://localhost:4200/framework/semantic-ui/component/button/count/1000/" +
 					"semantic-ui:button",
 				"http://localhost:4200/framework/dojo/component/button/count/1000/" +
-					"dojo:button"
+					"dojo:button",
+				"http://localhost:4200/framework/kendo-ui/component/button/count/1000/" +
+					"kendo-ui:button"
 			]
 		}
 	}
