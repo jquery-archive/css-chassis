64a51ac31474f7b2c413f8d1767ca776f76228fd
Michael Arestad <marestad@gmail.com>
2015-03-15 17:07:20 -0600

    Typography: Initial typographic styles
    Fixes gh-16
    Closes gh-57

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function( grunt ) {
 require( "load-grunt-tasks" )( grunt );
 
 var config = {
+		htmllint: {
+			dist: {
+				options: {},
+				src: [ "demos/*.html" ]
+			}
+		},
 		autoprefixer: {
 			dist: {
 				options: {
@@ -44,7 +50,7 @@ var config = {
 			}
 		},
 		csslint: {
-			src: [ "dist/css/chassis.lint.css", "dist/css/chassis.lint.css" ]
+			src: [ "dist/css/chassis.lint.css", "dist/css/chassis.lint.css", "demos/demos.css" ]
 		},
 		jscs: {
 			all: [ "*.js", "performance/**/*.js" ]
@@ -181,7 +187,7 @@ grunt.initConfig( config );
 grunt.loadTasks( "tasks" );
 grunt.loadNpmTasks( "perfjankie" );
 grunt.registerTask( "default", [ "test" ] );
-grunt.registerTask( "test", [ "build", "jshint", "jscs", "csslint" ] );
+grunt.registerTask( "test", [ "build", "jshint", "jscs", "csslint", "htmllint" ] );
 grunt.registerTask( "build", [ "svg", "sass", "csscomb", "cssmin" ] );
 grunt.registerTask( "perf", [
 	"start-selenium-server",
