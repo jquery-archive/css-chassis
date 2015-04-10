d508bacd34bd2032aaf532323d599093538c22e8
Michael Arestad <marestad@gmail.com>
2015-04-09 19:25:43 -0600

    Build: Initial table styles

---
+++ scss/variables/tables.js
@@ -0,0 +1,25 @@
+( function( root, factory ) {
+	if ( typeof define === "function" && define.amd ) {
+		define( [ "./chassis" ], factory );
+	} else if ( typeof exports === "object" ) {
+		module.exports = factory( require( "./chassis" ) );
+	} else {
+		root.chassis = factory( root.chassis );
+	}
+}( this, function( chassis ) {
+
+chassis.table = {
+	"table-base": {
+		name: "Table Element",
+		value: {
+			"margin": "0 0 1em",
+			"font-size": "16px",
+			"thead-font-size": "12px",
+			"border": "1px solid #eee",
+			"padding": "12px"
+		}
+	}
+};
+
+return chassis;
+} ) );
