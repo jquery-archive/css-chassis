515abf2f1f0ddb5968bc217a0dd808f0898a65d5
Sarah Frisk <sarah@sarahfrisk.com>
2016-01-25 23:58:52 -0500

    Colors: Initial pass on Chassis  palette for component use.
    Closes gh-140

--- scss/variables/colors.js
+++ scss/variables/colors.js
@@ -8,42 +8,74 @@
 	}
 }( this, function( chassis ) {
 
-var colors = {
-	"chassis-gray-dark": {
-		"value": "#383838",
-		"name": "Chassis Gray - Dark"
-	},
-	"chassis-gray": {
-		"value": "#c1c1c1",
-		"name": "Chassis Gray"
-	},
-	"chassis-gray-light": {
-		"value": "#f2f2f2",
-		"name": "Chassis Gray - Light"
-	},
-	"chassis-blue-dark": {
-		"value": "#388695",
-		"name": "Chassis Blue - Dark"
-	},
-	"chassis-blue": {
-		"value": "#4fc0c8",
-		"name": "Chassis Blue"
-	},
-	"chassis-yellow": {
-		"value": "#fadf51",
-		"name": "Chassis Yellow"
-	}
-};
 chassis.colors = {
-	"background": colors[ "chassis-gray-dark" ],
-	"font": colors[ "chassis-gray-dark" ],
-	"link": colors[ "chassis-blue-dark" ],
-	"button": colors[ "chassis-blue" ],
-	"buttonText": colors[ "chassis-gray-light" ],
-	"blockquoteBorder": {
-		name: "Chassis - Blockquote color",
-		value: "#eee"
-	}
+	"default": {
+		name: "Default Colors",
+		value: {
+			"base": "#FAFAFA",
+			"light": "#FFFFFF",
+			"dark": "#FAFAFA",
+			"darker": "#F5F5F5"
+		}
+	},
+	"primary": {
+		name: "Primary Colors",
+		value: {
+			"base": "#3F51B5",
+			"light": "#5C6BC0",
+			"dark": "#3949AB",
+			"darker": "#303F9F"
+		}
+	},
+	"success": {
+			name: "Success Colors",
+			value: {
+				"base": "#43A047",
+				"light": "#4CAF50",
+				"dark": "#388E3C",
+				"darker": "#2E7D32"
+			}
+		},
+	 "warning": {
+			name: "Warning Colors",
+			value: {
+				"base": "#FF5722",
+				"light": "#FF7043",
+				"dark": "#F4511E",
+				"darker": "#E64A19"
+		 }
+	 },
+		"info": {
+			name: "Info Colors",
+			value: {
+				"base": "#039BE5",
+				"light": "#03A9F4",
+				"dark": "#0288D1",
+				"darker": "#0277BD"
+			}
+	 },
+	 "error": {
+			name: "Error Colors",
+			value: {
+				"base": "#F44336",
+				"light": "#EF5350",
+				"dark": "#E53935",
+				"darker": "#D32F2F"
+			}
+	 },
+	 "text": {
+			name: "Text Colors",
+			value: {
+				"base": "#212121",
+				"light": "#727272"
+			}
+	 },
+	 "background": {
+		 name: "Background Colors",
+		 value: {
+			"base": "#fff"
+		 }
+	 }
 };
 return chassis;
 } ) );
