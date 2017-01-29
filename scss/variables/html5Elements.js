b8403f6576eddfa978b98cf1e82aac4840892b6a
sumedh123 <blesssumedh@gmail.com>
2017-01-29 10:03:15 +0530

    New Components : HTML5 form elements

---
+++ scss/variables/html5Elements.js
@@ -0,0 +1,35 @@
+(function( root, factory ) {
+	if ( typeof define === "function" && define.amd ) {
+		define( [ "./chassis" ], factory );
+	} else if ( typeof exports === "object" ) {
+		module.exports = factory( require( "./chassis" ) );
+	} else {
+		root.chassis = factory( root.chassis );
+	}
+}( this, function( chassis ) {
+
+chassis.formelements = {
+	"searchbox" : {
+		name : "Search Input Styling",
+		value : {
+			"border-color" : "#212f3d",
+			"color" : "#566573"
+		}
+	},
+	"emailbox" : {
+		name : "Email Input Styling",
+		value : {
+			"valid" : {
+				"color" : "#566573",
+				"border-color" : "#212f3d"
+			},
+			"invalid" : {
+				"color" : "#566573",
+				"border-color" : "#e74c3c"
+			}
+		}
+	}
+};
+
+return chassis;
+} ) );
