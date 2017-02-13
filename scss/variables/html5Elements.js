60c3424a9dab33e0751a28d7b9b2cde3d85773eb
sumedh123 <blesssumedh@gmail.com>
2017-02-13 22:54:08 +0530

    New Components : update html5Elements.js to fix travis CI build fail

--- scss/variables/html5Elements.js
+++ scss/variables/html5Elements.js
@@ -1,4 +1,4 @@
-(function( root, factory ) {
+( function( root, factory ) {
 	if ( typeof define === "function" && define.amd ) {
 		define( [ "./chassis" ], factory );
 	} else if ( typeof exports === "object" ) {
